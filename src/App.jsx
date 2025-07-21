f3e8567ef0ea32b4fc364b369c4297e2a7036939
alaaSnayel <alaanayel111@gmail.com>
2025-07-21 11:17:55 +0300

    Displaying Toasts (Notifications)

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClint = new QueryClient({
   defaultOptions: {
@@ -40,6 +41,33 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+            style: {
+              background: "var(--color-green-700)",
+              color: "var(--color-grey-50)",
+            },
+          },
+          error: {
+            duration: 5000,
+            style: {
+              background: "var(--color-red-700)",
+              color: "var(--color-grey-50)",
+            },
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -57,7 +58,7 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("Cabin deleted successfully");
+      toast.success("Cabin deleted successfully");
 
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
@@ -65,7 +66,7 @@ function CabinRow({ cabin }) {
     },
 
     onError: (error) => {
-      alert(error.message);
+      toast.error(error.message);
     },
   });
 
