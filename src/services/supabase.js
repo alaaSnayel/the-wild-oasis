374829f096403bae8b5b6c8e8e760d22e53f1ee7
alaaSnayel <alaanayel111@gmail.com>
2025-08-03 15:36:51 +0300

    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://znzhernhedrengqhkazv.supabase.co";
+export const supabaseUrl = "https://znzhernhedrengqhkazv.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpuemhlcm5oZWRyZW5ncWhrYXp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MTg5MzQsImV4cCI6MjA2ODM5NDkzNH0.SkYBrH-yBDBE7_sg3LSRdulKDWsDqdcFQCMtkccrpSM";
 const supabase = createClient(supabaseUrl, supabaseKey);
