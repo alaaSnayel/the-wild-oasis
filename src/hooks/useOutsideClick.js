9d4fc8cb0ea500b763d49cab040afb117237854f
alaaSnayel <alaanayel111@gmail.com>
2025-09-18 14:11:23 +0300

    Building a Reusable Context Menu

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function useOutsideClick(ref, callback) {
   useEffect(() => {
@@ -13,4 +13,23 @@ export function useOutsideClick(ref, callback) {
       document.removeEventListener("click", handleClickOutside, true);
     };
   }, [ref, callback]);
+}
+
+export function useOutsideClick2(callback) {
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [callback]);
+
+  return ref;
 }
