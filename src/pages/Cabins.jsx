88139fdf7b7e7a351200dde4da9edc3d63c4f0da
alaaSnayel <alaanayel111@gmail.com>
2025-09-20 11:54:18 +0300

    Client-Side Filtering: Filtering Cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,14 +2,26 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
+import { useSearchParams } from "react-router-dom";
 // import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [searchParams] = useSearchParams();
+
+  const paramsValue = searchParams.get("discount") || "all";
+
+  let headerText;
+
+  if (paramsValue === "all") headerText = "All cabins";
+  if (paramsValue === "no-discount") headerText = "No-discount cabins";
+  if (paramsValue === "with-discount") headerText = "Discounted cabins";
+
   return (
     <>
       <Row type="horizontal">
-        <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <Heading as="h1">{headerText}</Heading>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />
