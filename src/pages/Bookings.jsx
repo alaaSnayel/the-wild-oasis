ed08d2b5edfabd3e94c4f6f0c3dc250ae2bc5f17
alaaSnayel <alaanayel111@gmail.com>
2025-09-30 15:04:31 +0300

    API-Side Filtering: Filtering Bookings

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,13 +1,25 @@
 import BookingTable from "../features/bookings/BookingTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import BookingTableOperations from "../features/bookings/BookingTableOperations";
+import { useSearchParams } from "react-router-dom";
 
 function Bookings() {
+  const [searchParams] = useSearchParams();
+  const paramsValue = searchParams.get("status") || "all";
+
+  let headerText;
+
+  if (paramsValue === "all") headerText = "All bookings";
+  if (paramsValue === "checked-out") headerText = "Checked out bookings";
+  if (paramsValue === "checked-in") headerText = "Checked in bookings";
+  if (paramsValue === "unconfirmed") headerText = "Unconfirmed bookings";
+
   return (
     <>
       <Row type="horizontal">
-        <Heading as="h1">All bookings</Heading>
-        <p>TEST</p>
+        <Heading as="h1">{headerText}</Heading>
+        <BookingTableOperations />
       </Row>
 
       <BookingTable />
