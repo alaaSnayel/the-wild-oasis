4ff8445043666c96776b90a68c99faea741834c9
alaaSnayel <alaanayel111@gmail.com>
2025-10-27 13:53:48 +0300

    Authentication: User Login With Supabase

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,12 +3,23 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+// import Spinner from "../../ui/Spinner";
+import SpinnerMini from "../../ui/SpinnerMini";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("alaa@gmail.com");
+  const [password, setPassword] = useState("Alaa123");
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({ email, password });
+  }
+
+  // if (isLoading) return <Spinner />;
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -16,6 +27,7 @@ function LoginForm() {
         <Input
           type="email"
           id="email"
+          disabled={isLoading}
           // This makes this form better for password managers
           autoComplete="username"
           value={email}
@@ -26,13 +38,16 @@ function LoginForm() {
         <Input
           type="password"
           id="password"
+          disabled={isLoading}
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {isLoading ? <SpinnerMini /> : "Login"}
+        </Button>
       </FormRowVertical>
     </Form>
   );
