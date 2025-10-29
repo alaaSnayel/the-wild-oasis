ddbf7e7c2a12d21383c5c18244eb13b8d96527b7
alaaSnayel <alaanayel111@gmail.com>
2025-10-29 10:17:43 +0300

    User Logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
     onSuccess: () => {
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log(err);
