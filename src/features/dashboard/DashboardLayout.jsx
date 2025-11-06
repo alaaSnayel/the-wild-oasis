847cf4621fca7efa69d21ccbf59d6c650cdb1145
alaaSnayel <alaanayel111@gmail.com>
2025-11-06 10:31:26 +0200

    Building the Dashboard Layout & Computing Recent Bookings and Stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,23 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading, bookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading || isLoading2) return <Spinner />;
+  console.log(bookings);
+  console.log(stays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today&apos;s activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
